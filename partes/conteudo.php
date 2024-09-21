af31f3506431f69285ea9310e0964ad0b59614af
Bruna Karini Martins Gonçalves <bruna.maia@sou.ufac.br>
2024-09-21 15:21:23 -0500

    Mudando o texto.

--- partes/conteudo.php
+++ partes/conteudo.php
@@ -1,6 +1,6 @@
 <img class="vagas" src="imagens/vagas.jpg" alt="Vagas de estacionamento">
 <div class="texto">
     <h1>Modernizando o Futuro dos Estacionamentos</h1>
-    <p>Um sistema automatizado que maximiza o uso do espaço, reduz erros e garante uma experiência eficiente para os clientes, aumentando a satisfação e a receita.</p>
+    <p>Um sistema automatizado que reduz erros, aumentando a satisfação do cliente e a receita.</p>
     <a href="veiculo.php">CADASTRAR VEÍCULO</a>
 </div>
