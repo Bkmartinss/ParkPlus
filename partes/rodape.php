c54c4ec5e80e8b3c0490e52b73381f961284c186
Felipe Oliveira <156553180+OliveiraFelipe98@users.noreply.github.com>
2024-09-29 18:11:21 -0400

    Update rodape.php adicionando link do instagram

--- partes/rodape.php
+++ partes/rodape.php
@@ -1,5 +1,5 @@
 <li class="pe">
-    <a href="">INSTAGRAM</a>
+    <a href="https://www.instagram.com/uninorteacre/">INSTAGRAM</a>
     <a href="">WHATSAPP</a>
 </li>
 <p>Created by FJMB_Solutions</p>
