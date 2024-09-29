bc5331b48ff75673708169417cb22e05cf60f60a
Felipe Oliveira <156553180+OliveiraFelipe98@users.noreply.github.com>
2024-09-29 18:17:47 -0400

    Update rodape.php Link whatsapp

--- partes/rodape.php
+++ partes/rodape.php
@@ -1,5 +1,5 @@
 <li class="pe">
     <a href="https://www.instagram.com/uninorteacre/">INSTAGRAM</a>
-    <a href="">WHATSAPP</a>
+    <a href="https://w.app/ParkPlus">WHATSAPP</a>
 </li>
 <p>Created by FJMB_Solutions</p>
